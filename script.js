c9979a350c1d12a8221c9e28dc2ace580760f243
sumafujiko <sumafujiko@gmail.com>
2024-04-21 21:39:42 +0100

    added more images to rng gallery

--- script.js
+++ script.js
@@ -30,21 +30,36 @@ let images = [
   '../images/about-3.jpeg',
   '../images/home-1.jpeg',
   '../images/home-2.jpeg',
-  '../images/home-3.jpeg', 
+  '../images/home-3.jpeg',
   '../images/home-3.jpeg',
   '../images/home-bottom-1.jpeg',
   '../images/home-bottom-2.jpeg',
-  '../images/home-bottom-3.jpeg'
-]; 
-function showRandomImage() {
-  let randomIndex = Math.floor(Math.random() * images.length); 
-  let selectedImage = images[randomIndex]; 
+  '../images/home-bottom-3.jpeg',
+  '../images/shop-images/board-game.jpeg',
+  '../images/shop-images/cap.jpeg',
+  '../images/shop-images/cover.jpeg',
+  '../images/shop-images/dress.jpeg',
+  '../images/shop-images/figurines.jpeg',
+  '../images/shop-images/hat.jpeg',
+  '../images/shop-images/keychain.jpeg',
+  '../images/shop-images/necklace.jpeg',
+  '../images/shop-images/phone-case.jpeg',
+  '../images/shop-images/shirt.jpeg',
+  '../images/shop-images/shirt1.jpeg',
+  '../images/shop-images/sneakers.jpeg',
+  '../images/shop-images/socks.jpeg',
+  '../images/shop-images/stickers.jpeg',
+  '../images/shop-images/sweaters.jpeg',
+];
 
-  
+function showRandomImage() {
+  let randomIndex = Math.floor(Math.random() * images.length);
+  let selectedImage = images[randomIndex];
+
   let imgElement = document.getElementById('randomWubboImage');
   imgElement.src = selectedImage;
+
+  imgElement.classList.remove('d-none');
 }
 
-// Add event listener to button
 document.getElementById('randomWubbo').addEventListener('click', showRandomImage);
-
