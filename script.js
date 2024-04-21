e0aa16dd2963c3821b912f0b04ffd03d4379ee02
rivarien <jirurivarien@gmail.com>
2024-04-21 22:11:38 +0100

    Favicon Added

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ let images = [
   '../images/shop-images/sneakers.jpeg',
   '../images/shop-images/socks.jpeg',
   '../images/shop-images/stickers.jpeg',
-  '../images/shop-images/sweaters.jpeg',
+  '../images/shop-images/sweater.jpeg',
 ];
 
 function showRandomImage() {
