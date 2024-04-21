87ecc5e801ef1d9c65d320fbc2c0d63285c570f2
sumafujiko <sumafujiko@gmail.com>
2024-04-21 21:07:09 +0100

    Initial function for random image display

--- script.js
+++ script.js
@@ -5,7 +5,6 @@
 
     var forms = document.querySelectorAll('.needs-validation')
   
-   
     Array.prototype.slice.call(forms)
       .forEach(function (form) {
         form.addEventListener('submit', function (event) {
@@ -26,4 +25,26 @@ function apolgise(){
     alert("We are currently not accepting contact forms. However we appreciate your opinions and custom! Check back again soon.");
 }
 
+let images = [
+  './images/about-1.jpeg',
+  './images/about-3.jpeg',
+  './images/home-1.jpeg',
+  './images/home-2.jpeg',
+  './images/home-3.jpeg', 
+  './images/home-3.png',
+  './images/home-bottom-1.jpeg',
+  './images/home-bottom-2.jpeg',
+  './images/home-bottom-3.jpeg'
+]; 
+function showRandomImage() {
+  let randomIndex = Math.floor(Math.random() * images.length); 
+  let selectedImage = images[randomIndex]; 
+
+  
+  let imgElement = document.getElementById('randomWubboImage');
+  imgElement.src = selectedImage;
+}
+
+// Add event listener to button
+document.getElementById('randomWubbo').addEventListener('click', showRandomImage);
 
